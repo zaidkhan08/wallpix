1a1667cf15e40dcbad8853db71bb2050a2f13904
pc <idhackkaregabhadk@gmail.com>
2024-02-27 17:26:34 +0530

    27/2/24(2)

--- lib/main.dart
+++ lib/main.dart
@@ -81,6 +81,12 @@ class _MyHomePageState extends State<MyHomePage> {
                     color: Colors.black,
                   ),
                 ),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage("https://images.unsplash.com/photo-1552250575-e508473b090f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"), // Set your image URL here
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
               Expanded( // Wrap ListView with Expanded
                 child: ListView(
